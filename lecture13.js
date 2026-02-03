8feecacfc3e27a1e23151436e4121b32193b1318
owaiskhan2020 <owaiskhanqq2020@gmail.com>
2026-02-03 15:35:26 +0500

    operator and type

---
+++ lecture13.js
@@ -0,0 +1,86 @@
+// assignment operator in js
+// + operator
+
+let num1 = 10;
+let num2 = 5;
+
+let result = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${result}`)
+
+// - Subtraction operator
+let x = 10;
+let y = 7;
+let z = x-y;
+console.log(`the subtraction of ${x} and  ${y} is ${z}`)
+
+// * Multiplication operator
+let a = 9;
+let b = 5;
+let c = a*b;
+console.log(`The multiplication of ${a} and ${b} is ${c}`)
+
+// **  Exponentiation operator
+let d = 10;
+let e = 6;
+let f = d**e;
+console.log(`The exponentiation of ${d} and ${e} is ${f}`)
+//Division operator
+let g = 11;
+let h = 9;
+let i = g/h;
+console.log(`The division of ${g} and ${h} is ${i}`)
+
+// Increment and Decrement
+let num = 21;
+console.log(num++)
+console.log(num--)
+
+if(num%2 == 0){
+    console.log(`the ${num} is even`)
+} else {
+    console.log(`the ${num} is not even`)
+}
+
+// js string addition
+
+let fname = "jhon";
+let lname = "doi";
+
+console.log(fname +' '+ lname)
+let num3 = 5;
+let num4 = "5";
+console.log(typeof(num3) +" "+ typeof(num4))
+
+// = operator
+let a1 = 10;
+console.log(a1)
+
+//+= operator
+let b1 = 12;
+ b1 += 3;
+ console.log(b1);
+  
+ // -= operator
+ let c1= 14;
+ c1 -= 6;
+ console.log(c1)
+
+ //*= operator
+ let d1 = 15;
+ d1 *= 7;
+ console.log(d1)
+
+ // /=operator
+ let e1 = 16;
+ e1 /=8;
+ console.log(e1)
+
+ // %= operator
+ let f1 = 16;
+ f1 %= 3;
+ console.log(f1)
+
+ // **= operator
+ let g1 = 21;
+ g1 **= 6;
+ console.log(g1)
