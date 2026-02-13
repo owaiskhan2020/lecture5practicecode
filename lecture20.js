2863028f65f066cdbb00286729276ae27c328088
owaiskhan2020 <owaiskhanqq2020@gmail.com>
2026-02-13 15:26:06 +0500

    for loop and while and do while

--- lecture20.js
+++ lecture20.js
@@ -14,11 +14,46 @@ const fruits =["apple","banana","cherry"];
 for(let x=0;x<fruits.length; x++){
     console.log(fruits[x]);
 }
+// const cars=["BMW","VoIvo","Saab","Ford"];
+// let len =cars.length;
+// for(let i5=0;i5<len;i5++){
+//     console.log(cars)
+// }
+// const cars = ["BMw","VoIvo","Saab","Ford"];
+// let len = cars.length;
+// let i4=2;
+// for(; i4<len;i4++ ){
+//     console.log(cars);    
+// }
+const cars= ["BMW","VoIvo","Saab","Ford"];
+let len = cars.length;
+let i6=3;
+for(;i6<len;){
+    console.log(cars);
+    i6++;
+}
+let i11= 3;
+const time=["1","2","3","4"];
+for(;time[i11];){
+    console.log(time);
+    i11++
+}
 // while loop
 let i3 = 0;
 while(i3<10){
     console.log(`the sum value is while loop :${i3}`);
     i3++
+}
+let i8 = 0;
+while(i8 < 10){
+    console.log(`The number is :${i8}`);
+    i8++
+}
+let i10=3
+const names=["owais","youman","nouman","suleman"];
+while(names[i10]){
+    console.log(names);
+    i10++
 }
 //do while
 let i2 = 0;
@@ -27,4 +62,12 @@ do{
 }while(i2 > 10){
     console.log(`the sum value is do while :${i2}`)
     i2++;
+}
+
+let i9 = 0;
+do{
+    console.log(`welcome`)
+}while(i9>10){
+    console.log(i9);
+    i9++
 }
