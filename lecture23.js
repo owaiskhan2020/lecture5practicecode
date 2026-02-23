0610c332364058bf0bf1410d4604990d766ac358
owaiskhan2020 <owaiskhanqq2020@gmail.com>
2026-02-23 12:33:34 +0500

    string search

---
+++ lecture23.js
@@ -0,0 +1,31 @@
+//------------indexof-------------
+let text = "Hello javaScript";
+console.log(text.indexOf("java"));
+
+//-----------lastindexof-----------
+let text1 = "hello world hello";
+console.log(text1.lastIndexOf("hello"));
+
+//-----------search----------
+let text2 = "i love javascript";
+console.log(text2.search("javascript"));
+
+//-------------math()------
+let text3 = "the rain in spain";
+console.log(text3.match("ain"));
+
+//-------------mathall-------
+// let text4 = "test1,test2,test3";
+// let matches = text4.matchAll(/test\d/g)
+
+//-----------includes()------
+let text5= "hello world";
+console.log(text5.includes("world"));
+
+///------------startsWith()------
+let text6 = "javascript is powerful";
+console.log(text6.startsWith("javascript"));
+
+// -----------endsWith()--------
+let text7 = "image.png";
+console.log(text7.endsWith("png"));
