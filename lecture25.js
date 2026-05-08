c3ede5c15ea888cd8c30f8d9af4f4d38e1fb7c30
owaiskhan2020 <owaiskhanqq2020@gmail.com>
2026-05-08 15:29:24 +0500

    Array

--- lecture25.js
+++ lecture25.js
@@ -72,9 +72,13 @@ console.log(DB)
 //     console.log(arr[i]);
 // }
 //------------pracitce-----
-let arr1 = [1,2,3,4,5];
-for (let i=0;i<arr1.length; i++){
-    console.log(arr1[i]);
+// let arr1 = [1,2,3,4,5];
+// for (let i=0;i<arr1.length; i++){
+//     console.log(arr1[i]);
+// }
+let arr1 = [1,2,3,4];
+for (let i=0;i<arr.length; i++){
+    console.log(arr1[i])
 }
 //-------for of loop--------
 // let fruits = ["orange", "mango", "banana"];
